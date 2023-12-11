44f442be2db8859a77026698be5315daf41c6d69
Kishore kumar <kishore.poosa@syniti.com>
2023-12-11 14:41:39 -0800

    first step towards clan code

---
+++ CleanCode/ConsoleApp1/AreaCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class AreaCalculator
+    {
+        public double CalculateRectangleArea(double length, double width)
+        {
+            return length * width;
+        }
+
+    }
+}
---
+++ CleanCode/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Console.WriteLine("Hello, World!");
+
+var areaCalculator = new AreaCalculator();
+double area = areaCalculator.CalculateRectangleArea(5, 10);
+
+Console.WriteLine(area);
